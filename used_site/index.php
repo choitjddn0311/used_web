05a8972dfb92788172d55095916cba417545606b
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-11-29 16:45:19 +0900

    Add files via upload

---
+++ used_site/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>내안에 중고 | 내 손 안에서 모든것을</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <header>
+        <div class="header_inner">
+            <nav class="gnb">
+                <ul>
+                    <li>매물 보기</li>
+                    <li>시세 조회</li>
+                    <li>찜한 상품</li>
+                </ul>
+            </nav>
+            <div class="account">
+                <a href="login.php">
+                    <button>로그인</button>
+                </a>
+                <a href="join.php">
+                    <button>회원가입</button>
+                </a>
+            </div>
+        </div>
+    </header>
+    
+</body>
+</html>
