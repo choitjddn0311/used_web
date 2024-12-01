50babee4555bd1045dd4468b8eb09090de82fe30
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-12-02 00:52:33 +0900

    feat: 로그인 기능 추가 아직 구현이 덜 된 부분이 있음

--- used_site/login.php
+++ used_site/login.php
@@ -7,10 +7,38 @@
     <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
+    <?php
+    session_start();
+        $conn = mysqli_connect('localhost' , 'root' , '' , 'used_platform');
+
+        if($_SERVER['REQUEST_METHOD']=='POST'){
+            $id = $_POST['id'];
+            $pw = $_POST['password'];
+
+            $sql = "SELECT * FROM user_info WHERE id='$id'";
+            $result = mysqli_query($conn, $sql);
+
+            if(mysqli_num_rows($result) > 0){
+                $row = mysqli_fetch_assoc($result);
+
+                if(password_verify($pw, $row['password'])) {
+                    $_SESSION['user_id'] = $row['id'];
+                    $_SESSION['user_name'] = $row['name'];
+
+                    header('Location: index.php');
+                    exit;
+                } else {
+                    echo "<script>alert('비밀번호가 잘못되었습니다')</script>";
+                }
+            } else {
+                echo "<script>alert('아이디가 존재하지 않습니다.')</script>";
+            }
+        };
+    ?>
     <main class="login_container">
         <p>내 손안에 중고</p>
         <div class="login_box">
-            <form action="index.php">
+            <form action="login.php" method="POST">
                  <div class="info_inputBox">
                     <input type="text" name="id" placeholder="아이디를 입력해주세요.">
                     <input type="password" name="password" placeholder="비밀번호를 입력해주세요.">
