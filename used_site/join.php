8e2a882f4037fdbc407005ca5a19aa0b5ad9be8a
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-12-02 00:52:18 +0900

    feat: 회원가입 기능추가

--- used_site/join.php
+++ used_site/join.php
@@ -4,8 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>내안에 중고 회원가입</title>
+    <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
-    
+    <?php
+        $conn = mysqli_connect('localhost' , 'root' , '' , 'used_platform');
+    ?>
+<main class="join_container">
+        <p>내 손안에 중고</p>
+        <div class="join_box">
+            <form action="insert.php" method="POST">
+                 <div class="join_info">
+                    <input type="text" name="name" placeholder="이름을 입력해주세요.">
+                    <input type="text" name="id" placeholder="아이디를 입력해주세요.">
+                    <input type="password" name="password" placeholder="비밀번호를 입력해주세요.">
+                    <input type="password" name="repassword" placeholder="비밀번호를 다시 입력해주세요.">
+                    
+                 </div>
+                 <input type="submit" value="회원가입" class="join_btn" >
+            </form>
+        </div>
+    </main>
 </body>
 </html>
