75af9fd40e702cb36256679193558519e9552d91
choitjddn0311 <cswcjw311427@naver.com>
2024-12-03 17:35:33 +0900

    feat: 로그인 비로그인 화면 구ㅡ현

--- used_site/index.php
+++ used_site/index.php
@@ -7,6 +7,12 @@
     <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
+    <?php
+        session_start();
+        $conn = mysqli_connect('localhost' , 'root' , '' , 'used_platform');
+
+        $is_logged_in = isset($_SESSION['user_id']);
+    ?>
     <header>
         <div class="header_inner">
             <nav class="gnb">
@@ -18,12 +24,19 @@
                 </ul>
             </nav>
             <div class="account">
-                <a href="login.php">
+                <?php if ($is_logged_in): ?>
+                    <span><?=htmlspecialchars($_SESSION['user_id'])?>님</span>
+                    <a href="logout.php">
+                        <button>로그아웃</button>
+                    </a>
+                <?php else: ?>
+                    <a href="login.php">
                     <button>로그인</button>
                 </a>
                 <a href="join.php">
                     <button>회원가입</button>
                 </a>
+                <?php endif; ?>
             </div>
         </div>
     </header>
--- used_site/login_insert.php
+++ used_site/login_insert.php
@@ -14,4 +14,22 @@
             </script>";
         exit;
     }
+
+    $result = mysqli_query($conn, $sql);
+    $user = mysqli_fetch_assoc($result);
+
+    if($user) {
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['user_name'] = $user['name'];
+        echo "
+        <script>
+            alert('로그인 성공!');
+            location.href = 'index.php';
+        </script>";
+    } else {
+        "<script>
+            alert('아이디 또는 비밀번호가 일치하지 않습니다');
+            location.href = 'login.php';
+        </script>";
+    }
 ?>
