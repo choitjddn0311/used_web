71d4120fcbb9536ad28b3d4c7583639b8682e223
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-12-03 23:31:29 +0900

    Docs: add comments

--- used_site/join_insert.php
+++ used_site/join_insert.php
@@ -1,26 +1,33 @@
 <?php
+
+// sql db연동
         $conn = mysqli_connect('localhost' , 'root' , '' , 'used_platform');
+        // table에 잇는 모든 값 불러오기
         $name = mysqli_real_escape_string($conn, $_POST['name']);
         $id = mysqli_real_escape_string($conn, $_POST['id']);
         $pw = mysqli_real_escape_string($conn, $_POST['password']);
         $rpw = mysqli_real_escape_string($conn , $_POST['repassword']);
-        $sql = "INSERT INTO user_info(name,id,pw) VALUES ('$name' , '$id' , '$pw')";
-
-    if($pw !== $rpw) {
-        echo "<script>
-                alert('비밀번호가 일정하지 않습니다.');
-                location.href='join.php';
+        
+        // 회원가입시 비번일치 확인하기
+        if($pw !== $rpw) {
+            echo "<script>
+            alert('비밀번호가 일정하지 않습니다.');
+            location.href='join.php';
             </script>";
-        exit;
-    };
-
+            exit;
+        };
+        
+        // insert문으로 table에 정보넣기
     $sql = "INSERT INTO user_info(name, id, pw) VALUES ('$name' , '$id' , '$pw')";
 
+    // mysql query문 집어넣고 if문으로 확인하기
+    // 회원가입 성공시 
     if(mysqli_query($conn , $sql)) {
         echo "<script>
                 alert('회원가입이 완료되었습니다.');
                 location.href='index.php';
             </script>";
+    //회원가입 실패시
     } else {
         echo "<script>
                 alert('회원가입을 실패하셨습니다.');
--- used_site/login_insert.php
+++ used_site/login_insert.php
@@ -1,12 +1,15 @@
 <?php
+    //세션시작
     session_start();
+    //db연결
     $conn =  mysqli_connect('localhost' , 'root' , '' , 'used_platform');
-
+    //값 가져오기
     $name = mysqli_real_escape_string($conn , $_POST['name']);
     $id = mysqli_real_escape_string($conn , $_POST['id']);
     $pw = mysqli_real_escape_string($conn , $_POST['password']);
     $sql = "SELECT * FROM user_info WHERE id = '$id'";
 
+    //input창 비어있는지 확인하기
     if(empty($id) || empty($pw)){
         echo "<script>
                 alert('아이디 또는 비밀번호를 입력해주세요.');
@@ -14,10 +17,12 @@
             </script>";
         exit;
     }
-
+    // mysql 쿼리실행
     $result = mysqli_query($conn, $sql);
+    // query를 통해 얻은 값을 1개씩 리턴
     $user = mysqli_fetch_assoc($result);
 
+    // $session 으로 저장
     if($user) {
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['user_name'] = $user['name'];
