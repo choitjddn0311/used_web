a4ef57b87b9b7e09a8ce367f4d1fec6173dd474c
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-12-03 23:37:47 +0900

    Docs: add comments

--- used_site/logout.php
+++ used_site/logout.php
@@ -1,7 +1,11 @@
 <?php
+    //세션시작
     session_start();
+    //세션에 등록되어있는 모든 변수값 삭제
     session_unset();
+    //현재세션종료
     session_destroy();
+    //후 index.php로 location이동
     header("Location: index.php");
     exit;
 ?>
