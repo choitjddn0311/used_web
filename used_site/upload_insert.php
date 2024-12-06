2b675c8c9f1212102b9333a53a79065723bd510f
choitjddn0311 <cswcjw311427@naver.com>
2024-12-06 16:36:51 +0900

    feat: upload

--- used_site/upload_insert.php
+++ used_site/upload_insert.php
@@ -4,10 +4,35 @@
     if($_SERVER['REQUEST_METHOD'] == 'post'){
         $title = $_POST['title'];
         $content = $_POST['content'];
-        $price = $_POST['price'];
+        $price = intval($_POST['price']);
+    }
+    if(empty($title) || empty($content) || empty($content)){
+        echo "
+        <script>
+            alert('비어있는 내용을 입력해주세요.');
+            location.href = 'upload.php';
+        </script>";
+        exit;
     }
 
-    $sql = "INSERT INTO title,content,price VALUE ('$title' , '$content' , '$price')";
+    $_SESSION['title'] = $title;
+    $_SESSION['content'] = $content;
+    $_SESSION['price'] = $price;
 
+    $sql = "INSERT INTO products(title,content,price) VALUE ('$title' , '$content' , '$price')";
     $result = mysqli_query($conn , $sql);
+
+    if($result) {
+        echo "
+        <script>
+            alert('업로드를 마쳤습니다');
+            location.href = 'index.php';
+        </script>";
+    } else {
+        echo "
+        <script>
+            alert('업로드 중 오류가 발생했습니다.');
+            location.href = 'upload.php';
+        </script>";
+    }
 ?>
